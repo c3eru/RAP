33c332998c44e987716211f27a56b0f663c06eb9
c3eru <96016081+c3eru@users.noreply.github.com>
2022-12-22 21:55:06 +0700

    Test

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ export KBUILD_BUILD_USER=$KBUILD_BUILD_USER
 export KBUILD_BUILD_HOST=$KBUILD_BUILD_HOST
 export BUILD_USERNAME=$KBUILD_BUILD_USER
 export BUILD_HOSTNAME=$KBUILD_BUILD_HOST
+export WITH_GMS=true
 lunch lineage_spes-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
