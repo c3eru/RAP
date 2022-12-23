2b79c7b9629ff3b0a6ec780390d987fff3b54b05
c3eru <96016081+c3eru@users.noreply.github.com>
2022-12-24 00:16:41 +0700

    Test

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
 git clone --depth 1 https://github.com/c3eru/local_manifest -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
