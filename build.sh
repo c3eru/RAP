4824b1e3e8e4acc19df6237d163446b2e04f6849
c3eru <96016081+c3eru@users.noreply.github.com>
2023-02-22 19:34:24 +0700

    [skip ci]

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ export KBUILD_BUILD_USER=$KBUILD_BUILD_USER
 export KBUILD_BUILD_HOST=$KBUILD_BUILD_HOST
 export BUILD_USERNAME=$KBUILD_BUILD_USER
 export BUILD_HOSTNAME=$KBUILD_BUILD_HOST
+export WITH_GMS=true
 lunch derp_citrus-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
