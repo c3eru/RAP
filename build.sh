1d9e4449e356e290953f65d4e7420f4cb5c29b53
c3eru <96016081+c3eru@users.noreply.github.com>
2023-03-25 11:57:22 +0700

    [skip ci]

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone --depth 1 https://github.com/MOBX-PROJECTS/local_manifests -b chime-banana .repo/local_manifests
+git clone --depth 1 https://github.com/c3eru/local_manifest -b citrus-banana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -13,7 +13,7 @@ export KBUILD_BUILD_USER=$KBUILD_BUILD_USER
 export KBUILD_BUILD_HOST=$KBUILD_BUILD_HOST
 export BUILD_USERNAME=$KBUILD_BUILD_USER
 export BUILD_HOSTNAME=$KBUILD_BUILD_HOST
-lunch banana_chime-userdebug
+lunch banana_citrus-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
