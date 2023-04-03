a993420506f8b7d1dc8de86805f028cbb1040e3c
c3eru <96016081+c3eru@users.noreply.github.com>
2023-04-03 19:23:49 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone --depth 1 https://github.com/c3eru/local_manifest -b citrus-banana .repo/local_manifests
+git clone --depth 1 https://github.com/c3eru/local_manifest -b citrus-banana-1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
